8c2275c13128214bd29600e83d6a65a0eef0c2e5
Kenji Eva <KenjiE20@yahoo.co.uk>
2009-12-22 14:54:41 +0000

    Use a for loop, for the file paths

--- cleanup.sh
+++ cleanup.sh
@@ -1,28 +1,26 @@
 #! /bin/bash
 
 dstamp=$(date +%c)
-thum_file="/tmp/thumbs_results"
-dsst_file="/tmp/dsstore_results"
-dxva_file="/tmp/dxva_results"
+
+paths=(/media/scratch /media/store /media/shares)
+
+thum_file="/tmp/thumbs_log"
+dsst_file="/tmp/dsstore_log"
+dxva_file="/tmp/dxva_log"
 
 echo $dstamp > $thum_file
 echo $dstamp > $dsst_file
 echo $dstamp > $dxva_file
 
-find /media/scratch/ -path /media/scratch/lost+found -prune -o -writable -type f -name Thumbs.db -print0 | du --files0-from=- -hc >> $thum_file
-find /media/scratch/ -path /media/scratch/lost+found -prune -o -writable -type f -name .DS_Store -print0 | du --files0-from=- -hc >> $dsst_file
-find /media/scratch/ -path /media/scratch/lost+found -prune -o -writable -type f -name dxva_sig.txt -print0 | du --files0-from=- -hc >> $dxva_file
+for i in "${paths[@]}" ; do
+    echo "Files found in $i:" >> $thum_file
+    find $i -path $i/lost+found -prune -o -writable -type f -name Thumbs.db -print0 | du --files0-from=- -hc >> $thum_file
+    echo "Files found in $i:" >> $dsst_file
+    find $i -path $i/lost+found -prune -o -writable -type f -name .DS_Store -print0 | du --files0-from=- -hc >> $dsst_file
+    echo "Files found in $i:" >> $dxva_file
+    find $i -path $i/lost+found -prune -o -writable -type f -name dxva_sig.txt -print0 | du --files0-from=- -hc >> $dxva_file
+done
 
 #find /media/scratch/ -type f -name .DS_Store -exec rm {} \;
 
-#find /media/store/ -writable -type f -name Thumbs.db -print0 | du --files0-from=- -hc >> $thum_file
-#find /media/store/ -writable -type f -name .DS_Store -print0 | du --files0-from=- -hc >> $dsst_file
-#find /media/store/ -writable -type f -name dxva_sig.txt -print0 | du --files0-from=- -hc >> $dxva_file
-
-#find /media/store/ -type f -name .DS_Store -exec rm {} \;
-
-#find /media/shares/ -writable -type f -name Thumbs.db -print0 | du --files0-from=- -hc >> $thum_file
-#find /media/shares/ -writable -type f -name .DS_Store -print0 | du --files0-from=- -hc >> $dsst_file
-#find /media/shares/ -writable -type f -name dxva_sig.txt -print0 | du --files0-from=- -hc >> $dxva_file
-
-#find /media/shares/ -type f -name .DS_Store -exec rm {} \;
+#rm $thum_file $dsst_file $dxva_file
